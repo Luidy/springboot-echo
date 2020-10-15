0db7082c111b01b64e9da7a083b98154b4a62ac8
DESKTOP-7CDDM0K\ejjo3 <ejjo321@gmil.com>
2020-10-16 00:08:29 +0900

    add. rabbitmq config

--- src/main/java/com/ejjo/echos/EchosApplication.java
+++ src/main/java/com/ejjo/echos/EchosApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EchosApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EchosApplication.class, args);
 	}
-
 }
